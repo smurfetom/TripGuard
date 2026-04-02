93f2e5dced421d9d13ca164821ea9450a1333435
Tom Erik Nilsen <tommen@Tom-sin-MacBook-Air.local>
2026-04-02 17:27:22 +0200

    feat(diag): expose DiagnosticsScreen export

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export { SetupScreen } from './SetupScreen';
 export { DrillerScreen } from './DrillerScreen';
 export { MirrorScreen } from './MirrorScreen';
 export { StatusScreen } from './StatusScreen';
+export { DiagnosticsScreen } from './DiagnosticsScreen';
